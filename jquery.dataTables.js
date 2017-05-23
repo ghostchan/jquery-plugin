a2d96bea698d9dc93dd3c19035bb5d10dbe0b9c2
chenzhi03 <chenzhi03@zuche.com>
2017-05-23 08:46:02 +0800

    分页和表格

---
+++ jquery.dataTables.js
@@ -0,0 +1,258 @@
+/**
+ * Created by hxd on 2017/3/7.
+ */
+/*eslint-disable */
+(function ($) {
+    var dataTable = {
+        init: function (el, options) {
+            var o = $.extend({
+                data:[],
+                columns: {},
+                operations:{
+                    displayed: false,
+                    title: "操作",
+                    btn: []
+                },
+                selectable: false,
+                selectBackFn: function (e) {
+                    // 选中回调
+                },
+                sortBy:"",
+                sortSign:"",
+                sortBackFn: function (e) {
+                    // 排序回调
+                },
+                onInit: function () {
+                    // 初始化回调
+                },
+                afterRender: function () {
+                    // 渲染后执行
+                }
+            }, options || {});
+            this.options = o;
+            this.el = el;
+            el.data('tableOptions', this);
+            o.onInit();
+            this._draw();
+            o.afterRender();
+            return this;
+        },
+
+        destroy: function () {
+            this.el.empty();
+        },
+
+        refresh: function () {
+            this._draw();
+        },
+
+        sort: function (key, sign) {
+            this.el.find("[data-key='" + key + "']").find(".js_sort" + sign + "").addClass("active");
+        },
+
+        //绘制table
+        _draw: function () {
+            this.destroy();
+            var self = this;
+            var columns = this.options.columns;
+            var operations = this.options.operations;
+            var data = this.options.data;
+            //<thead>元素
+            var thead = document.createElement("thead");
+            var thead_tr = document.createElement("tr");
+            thead_tr.className = "tb-title";
+            var tdCount = 0;
+            $.each(columns,function (key, item) {
+                var thead_tr_td = document.createElement("td");
+                if (typeof item == "string") {
+                    thead_tr_td.innerHTML = item;
+                } else {
+                    thead_tr_td.innerHTML = item.title;
+                    //Title class
+                    if (item && item.titleClass) {
+                        thead_tr_td.className = item.titleClass;
+                    }
+                    //列排序
+                    if (item && item.sortable) {
+                        var sortBy = item.sortBy ? item.sortBy : key;
+                        var sortSignUp = document.createElement("div");
+                        sortSignUp.className = "triangle-up js_sortUp";
+                        var sortSignDown = document.createElement("div");
+                        sortSignDown.className = "triangle-down js_sortDown";
+                        if (self.options.sortBy == sortBy) {
+                            if (self.options.sortSign == "Up") {
+                                sortSignUp.className = sortSignUp.className + " active";
+                            } else {
+                                sortSignDown.className = sortSignDown.className + " active";
+                            }
+                        }
+                        var sortSign = document.createElement("div");
+                        sortSign.className = "sortSign js_tableSort";
+                        sortSign.setAttribute("data-key", sortBy);
+                        sortSign.onclick = function () {
+                            var key = this.attributes.getNamedItem("data-key").nodeValue;
+                            self.options.sortBackFn(this, key);
+                        };
+                        sortSign.appendChild(sortSignUp);
+                        sortSign.appendChild(sortSignDown);
+                        thead_tr_td.appendChild(sortSign);
+                    }
+                }
+                thead_tr.appendChild(thead_tr_td);
+                tdCount++;
+            });
+            if (operations.displayed) {
+                var th_operate_td = document.createElement("td");
+                th_operate_td.innerHTML = operations.title;
+                thead_tr.appendChild(th_operate_td);
+                tdCount = tdCount + 1;
+            }
+            thead.appendChild(thead_tr);
+            self.el.append($(thead));
+            //<tbody>元素
+            var tbody = document.createElement("tbody");
+            if (data && data.length > 0) {
+                $.each(data, function (i, obj) {
+                    var tbody_tr = document.createElement("tr");
+                    var index = 0;
+                    $.each(columns,function (_key, item) {
+                        var value = obj[_key] == null ? "" : obj[_key];
+                        var td = document.createElement("td");
+                        //插入列
+                        if (self.options.selectable && index == 0) {
+                            td.className = "list-filter";
+                            var label = document.createElement("label");
+                            label.className = "js_radio";
+                            label.onclick = function () {
+                                self.options.selectBackFn(this);
+                            };
+                            var input = document.createElement("input");
+                            input.type = "radio";
+                            input.value = value;
+                            td.appendChild(label);
+                            td.appendChild(input);
+                            tbody_tr.appendChild(td);
+                        } else if (item && item.href) {
+                            var a = document.createElement("a");
+                            a.innerHTML = value;
+                            td.appendChild(a);
+
+                        } else {
+                            var span = document.createElement("span");
+                            span.innerHTML = value;
+                            td.appendChild(span);
+                        }
+                        //注入元素属性
+                        var td_first =  td.firstElementChild || td.firstChild;
+                        var td_last = td.lastElementChild || td.lastChild;
+                        if (td.className == "list-filter") {
+                            if (item && item.itemClass) {
+                                td_last.className = item.itemClass;
+                            }
+                        } else {
+                            if (item && item.itemClass) {
+                                td_first.className = item.itemClass;
+                            }
+                        }
+                        var attrObj = item && item.attr;
+                        if (attrObj) {
+                            $.each(attrObj,function (_k, _v) {
+                                if (_v.indexOf("{{:") > -1) {
+                                    for (var count = 0;count < 1;count++) {
+                                        var start = _v.indexOf("{{:") + 3;
+                                        var end = _v.indexOf("}}");
+                                        var keyWord = _v.substring(start, end);
+                                        var dataVal = typeof obj[keyWord] == "undefined" ? "" : obj[keyWord];
+                                        var _keyWord = "{{:" + keyWord + "}}";
+                                        _v = _v.replace(_keyWord, dataVal);
+                                        if (_v.indexOf("{{:") > -1) {
+                                            count--;
+                                        }
+                                    }
+                                }
+                                if (td.className == "list-filter") {
+                                    td_last.setAttribute(_k, _v);
+                                    td_first.setAttribute("for", td_last.id);
+                                } else {
+                                    td_first.setAttribute(_k, _v);
+                                }
+                            });
+                        }
+                        //注入td样式
+                        var styleObj = item && item.style;
+                        if (styleObj) {
+                            $.each(styleObj,function (_k, _v) {
+                                //td.style[_k] = _v;
+                                $(td).css(_k, _v);
+                            });
+                        }
+                        tbody_tr.appendChild(td);
+                        index++;
+                    });
+                    //操作按钮列
+                    if (operations.displayed) {
+                        var operate_td = document.createElement("td");
+                        $.each(operations.btn, function (b, btnItem) {
+                            var btn_a = document.createElement("a");
+                            btn_a.innerHTML = btnItem.title;
+                            if (btnItem.className) {
+                                btn_a.className = btnItem.className;
+                            }
+                            btn_a.setAttribute("href", "javascript:void(0)");
+                            if (btnItem.attr) {
+                                $.each(btnItem.attr,function (_k, _v) {
+                                    if (_v.indexOf("{{:") > -1) {
+                                        for (var count = 0;count < 1;count++) {
+                                            var start = _v.indexOf("{{:") + 3;
+                                            var end = _v.indexOf("}}");
+                                            var keyWord = _v.substring(start, end);
+                                            var dataVal = typeof obj[keyWord] == "undefined" || obj[keyWord] == null ? "" : obj[keyWord];
+                                            var _keyWord = "{{:" + keyWord + "}}";
+                                            _v = _v.replace(_keyWord, dataVal);
+                                            if (_v.indexOf("{{:") > -1) {
+                                                count--;
+                                            }
+                                        }
+                                    }
+                                    btn_a.setAttribute(_k, _v);
+                                });
+                            }
+                            btn_a.onclick = function () {
+                                btnItem.fn && btnItem.fn(this);
+                            };
+                            operate_td.appendChild(btn_a);
+                            if (b < operations.btn.length - 1) {
+                                var operate_span = document.createElement("span");
+                                operate_span.innerHTML = "&nbsp;&nbsp;|&nbsp;&nbsp;";
+                                operate_span.className = "gray";
+                                operate_td.appendChild(operate_span);
+                            }
+                        });
+                        tbody_tr.appendChild(operate_td);
+                    }
+                    tbody.appendChild(tbody_tr);
+                });
+            } else {
+                var tr_noData = document.createElement("tr");
+                tr_noData.className = "js_nodata";
+                var td_noData = document.createElement("td");
+                td_noData.setAttribute("colspan", tdCount.toString());
+                td_noData.innerHTML = "暂无记录";
+                tr_noData.appendChild(td_noData);
+                tbody.appendChild(tr_noData);
+            }
+            self.el.append($(tbody));
+        }
+    };
+
+    $.fn.dataTable = function (conf) {
+        // Method calling logic
+        if (dataTable[conf] && conf.charAt(0) != '_') {
+            return dataTable[conf].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof conf === 'object' || !conf) {
+            return dataTable.init(this, conf);
+        } else {
+            $.error('方法' +  conf + '在jQuery.dataTable中不存在！');
+        }
+    };
+})(jQuery);
---
+++ jquery.pagination.js
@@ -0,0 +1,251 @@
+/**
+ * Created by hxd on 2017/2/22.
+ */
+(function($){
+
+    var methods = {
+        init: function(el, options) {
+            var o = $.extend({
+                pageCount: 0,//总页数，不传默认为单页码显示
+                current: 1,//当前页码
+                displayedPages:5,//显示页码数
+                pageSize: 10,//单页显示条数
+                listLength: 1, //数据条数
+                ajaxRefresh:true,//是否为ajax刷新
+                textAlign: "right",//对齐方式
+                prevPageText: "上一页",
+                nextPageText:" 下一页",
+                firstPageText: "首页",
+                lastPageText: "尾页",
+                displayedHomeLast:false,//首尾页开关
+                displayedGo2Page:false,//跳转框开关
+                selectPageSize:false,//选择单页条数开关
+                disabled:false,//禁用开关
+                backFn: function(data) {
+                    //分页操作回调
+                },
+                onInit: function() {
+                    // 初始化回调
+                }
+            }, options || {});
+            this.options = o;
+            this.el = el;
+            el.data('pagination', this);
+            if(o.listLength == 0 && o.ajaxRefresh){
+                if(o.current == 1 && o.listLength == 0){
+                    this.destroy();
+                    return false;
+                }
+                if (typeof (o.backFn) == "function") {
+                    var _current = el.find("span.current").text();
+                    if(o.current - 1 <= el.find("span.current").text()){
+                        o.backFn(o.current - 1);
+                    }else{
+                        o.backFn(_current);
+                        alert("跳转页数已超过最大值");
+                    }
+                }
+            } else {
+                this._draw();
+                o.onInit();
+            }
+
+            return this;
+        },
+
+        prevPage: function() {
+            var o = this.options;
+            if (o.current > 1) {
+                this._selectPage(o.current - 1);
+            }
+        },
+
+        nextPage: function() {
+            var o = this.options;
+            if (parseInt(o.current) + 1 <= o.pageCount || (o.pageCount == 0 && o.pageSize == o.listLength)) {
+                this._selectPage(parseInt(o.current) + 1);
+            }
+        },
+
+        firstPage: function() {
+            this._selectPage(1);
+        },
+
+        lastPage: function() {
+            var o = this.options;
+            this._selectPage(o.pageCount);
+        },
+
+        getPagesCount: function() {
+            return this.options.pageCount;
+        },
+
+        setPagesCount: function(count) {
+            this.options.pageCount = count;
+        },
+
+        getCurrentPage: function () {
+            return this.options ? this.options.current : 1;
+        },
+
+        destroy: function(){
+            this.el.empty();
+        },
+
+        redraw: function(){
+            this._draw();
+        },
+
+        disable: function(){
+            this.options.disabled = true;
+            this._draw();
+        },
+
+        enable: function(){
+            this.options.disabled = false;
+            this._draw();
+        },
+
+        updatePageSize: function (pageSize) {
+            if(pageSize == this.options.pageSize){
+                return;
+            }
+            this.options.pageSize = pageSize;
+            this._selectPage(1);
+        },
+
+        getPageSize: function() {
+            return this.options ? this.options.pageSize : 10;
+        },
+
+        _draw: function() {
+            this.destroy();
+            var optionsHtml = '<option value="10"> 10 条/页</option>' +
+                '<option value="20"> 20 条/页</option>' +
+                '<option value="30"> 30 条/页</option>' +
+                '<option value="40"> 40 条/页</option>' +
+                '<option value="50"> 50 条/页</option>';
+            var args = this.options;
+            if(args.pageCount < 2 && args.pageCount != 0){
+                return false;
+            }
+            var $panel = this.el;
+            if(args.displayedHomeLast && args.pageCount > args.displayedPages) {
+                $panel.append('<a href="javascript:;" class="firstPage">'+ args.firstPageText +'</a><span class="page_link_border"> | </span>');
+            }
+            $panel.append('<a href="javascript:;" class="prevPage">'+ args.prevPageText +'</a>');
+            var currentPage = parseInt(args.current);
+            if (args.pageCount != 0) {
+                var steps = parseInt(args.displayedPages/2);
+                var listBegin = args.pageCount <= args.displayedPages ? 1 : (currentPage - steps) > 0 ? (currentPage + steps > args.pageCount ? args.pageCount - args.displayedPages + 1 : currentPage - steps) : 1; //开始页数
+                var listEnd = listBegin + args.displayedPages > args.pageCount ? args.pageCount : listBegin + args.displayedPages - 1; //结束页数
+                if(!args.displayedHomeLast && listBegin != 1 && args.displayedPages > 1){
+                    $panel.append('<a href="javascript:void(0);" class="tcdNumber">1</a>');
+                    if(listBegin - 1 != 1){
+                        $panel.append('<span class="tcdNumber">......</span>');
+                    }
+                }
+                while(listBegin <= listEnd){
+                    if (listBegin != args.current) {
+                        $panel.append('<a href="javascript:void(0);" class="tcdNumber">'+ listBegin +'</a>');
+                    } else {
+                        $panel.append('<span href="javascript:void(0);" class="tcdNumber current">'+ listBegin +'</span>');
+                    }
+                    listBegin++;
+                }
+                if(!args.displayedHomeLast && listEnd != args.pageCount && args.displayedPages > 1){
+                    if(listEnd  + 1 != args.pageCount){
+                        $panel.append('<span class="tcdNumber">......</span>');
+                    }
+                    $panel.append('<a href="javascript:void(0);" class="tcdNumber">'+ args.pageCount +'</a>');
+                }
+            }else {
+                $panel.append('<span href="javascript:void(0);" class="tcdNumber current">'+ currentPage +'</span>');
+            }
+            $panel.append('<a href="javascript:;" class="nextPage">'+ args.nextPageText +'</a>');
+            if (args.displayedHomeLast && args.pageCount > args.displayedPages) {
+                $panel.append('<span class="page_link_border"> | </span><a href="javascript:;" class="lastPage">'+ args.lastPageText +'</a>');
+            }
+
+            var ohtml = '<div class="operatePage">';
+            if (args.displayedGo2Page) {
+                ohtml += '<div class="gotoPage">' +
+                    '<input type="text" maxlength="6" class="whichPage"><button class="goToPage fz14">跳转</button>' +
+                    '</div>';
+            }
+            if (args.selectPageSize) {
+                if(args.disabled){
+                    ohtml += '<select class="selectPageSize" name="selectPageSize" disabled>' + optionsHtml + '</select>';
+                }else{
+                    ohtml += '<select class="selectPageSize" name="selectPageSize">' + optionsHtml + '</select>';
+                }
+            }
+            ohtml += '</div>';
+            $panel.append(ohtml);
+            $panel.find("[name='selectPageSize']").val(args.pageSize);
+            $panel.css("text-align",args.textAlign);
+            if(!args.disabled){
+                this._bindEvent();
+            }
+        },
+
+        _bindEvent: function () {
+            var self = this;
+            var obj = this.el;
+            obj.find("a.tcdNumber").unbind("click").bind("click",  function() {
+                var current = parseInt($(this).text());
+                self._selectPage(current);
+            });
+            obj.find("a.prevPage").unbind("click").bind("click",  function() {
+                self.prevPage();
+            });
+            obj.find("a.nextPage").unbind("click").bind("click",  function() {
+                self.nextPage();
+            });
+            obj.find("a.firstPage").unbind("click").bind("click",  function() {
+                self.firstPage();
+            });
+            obj.find("a.lastPage").unbind("click").bind("click",  function() {
+                self.lastPage();
+            });
+
+            obj.find("[name='selectPageSize']").unbind("change").bind("change",  function() {
+                var pageSize = this.value;
+                self.updatePageSize(pageSize);
+            });
+
+            obj.find(".goToPage").unbind("click").bind("click",  function() {
+                var pageNo = $(this).prev().val();
+                var _reg = /^[0-9]*[1-9][0-9]*$/;
+                var result = _reg.test($.trim(pageNo) );
+                if (result) {
+                    $(this).prev().removeClass("errorNo");
+                    self._selectPage(pageNo);
+                }
+            });
+        },
+
+        _selectPage: function(pageIndex, event) {
+            if (pageIndex > this.options.pageCount && this.options.pageCount > 0) {
+                return;
+            }
+            this.options.current = pageIndex;
+            if (!this.options.ajaxRefresh) {
+                this._draw();
+            }
+            return this.options.backFn(pageIndex, event);
+        }
+    };
+
+    $.fn.createPage = function(method) {
+        // Method calling logic
+        if (methods[method] && method.charAt(0) != '_') {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init(this, method);
+        } else {
+            $.error('方法' +  method + '在jQuery.pagination中不存在！');
+        }
+    };
+
+})(jQuery);
