0dfd09266f61a9bdc151893e3eeb9de802b431b0
chenzhi03 <chenzhi03@zuche.com>
2017-06-28 10:06:47 +0800

    placeholder

---
+++ placeholder/jquery.placeholder.js
@@ -0,0 +1,23 @@
+﻿$.fn.watermark = function(options) {
+	options = $.extend({watermarkClass: 'watermark'}, options || {});
+	return this.focus(function() {
+			var field = $(this);
+			if (field.val() == field.attr('data-label')) {
+				field.val('').removeClass(options.watermarkClass);
+			}
+		}).blur(function() {
+			var field = $(this);
+			if (field.val() == '') {
+				field.val(field.attr('data-label')).addClass(options.watermarkClass);
+			}
+		}).blur();
+};
+
+$.fn.clearWatermark = function() {
+	return this.each(function() {
+		var field = $(this);
+		if (field.val() == field.attr('data-label')) {
+			field.val('');
+		}
+	});
+};
